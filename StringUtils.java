bf1c7a0187a729f0461d49eb17cbf1e98df324f6
gg371 <54296664+gg371@users.noreply.github.com>
2019-11-17 12:16:21 -0600

    smartScrubHTML and scrubHTML work I think

--- StringUtils.java
+++ StringUtils.java
@@ -40,8 +40,47 @@ public class StringUtils {
 	    	return escapedString;
 	    }
 
-//	    public static String smartScrubHTML(String s) {
-//	    }
+	    public static String smartScrubHTML(String s) {
+	    	if(s == null) {
+	    		return null;
+	    	}
+	    	
+	    	String escapedString = "";
+	    	
+	    	for(int i = 0; i < s.length(); i++) {
+	    		if(s.charAt(i) == '&') {
+	    			if(s.charAt(i+1) == 'l' && s.charAt(i+2) == 't' && s.charAt(i+3) == ';') {
+	    				escapedString += "&";
+	    			}
+	    			else if(s.charAt(i+1) == 'g' && s.charAt(i+2) == 't' && s.charAt(i+3) == ';') {
+	    				escapedString += "&";
+	    			}
+	    			else if(s.charAt(i+1) == 'a' && s.charAt(i+2) == 'm' && s.charAt(i+3) == 'p' && s.charAt(i+4) == ';') {
+	    				escapedString += "&";
+	    			}
+	    			else if(s.charAt(i+1) == 'q' && s.charAt(i+2) == 'u' && s.charAt(i+3) == 'o' && s.charAt(i+4) == 't' && s.charAt(i+5) == ';') {
+	    				escapedString += "&";
+	    			}
+	    			else {
+	    				escapedString += "&amp;";
+	    			}
+	    		}
+	    		else if(s.charAt(i) == '<') {
+	    			escapedString += "&lt;";
+	    		}
+	    		else if(s.charAt(i) == '>') {
+	    			escapedString += "&gt;";
+	    		}
+	    		else if(s.charAt(i) == '"') {
+	    			escapedString += "&quot;";
+	    		}
+	    		else {
+		    		escapedString += s.charAt(i);
+	    		}
+	    	}
+	    	
+	    	return escapedString;
+	    }
 //
 //	    public static String formatPhoneNumber(String phone) {
 //	    }
