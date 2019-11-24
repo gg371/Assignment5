236fa74d070da32330933ce05fd176c63305eb32
gg371 <54296664+gg371@users.noreply.github.com>
2019-11-24 15:45:51 -0600

    DONE

--- string_utils.c
+++ string_utils.c
@@ -1,6 +1,6 @@
 /**
 * Author: Gracyn and Andrea
-* Date: 11/12/19
+* Date: 11/24/19
 * These are the code for the functions defined in string_utils.h
 */
 #include <stdlib.h>
@@ -19,9 +19,11 @@ char * scrubHTML(const char *str){
   int stringLength = strlen(str);
   int newStringLength = stringLength;
 
-  /*this for loop goes through the passed in string and stringLengths the number of times
-  one of the special characters shows up to calculate the extra amount of memory needed for the
-  length of the new string.*/
+  /*
+  * this for loop goes through the passed in string and stringLengths the number of times
+  * one of the special characters shows up to calculate the extra amount of memory needed for the
+  * length of the new string.
+  */
   int i = 0;
   for(i = 0; i < stringLength; i++){
     if(str[i] == '&'){
@@ -31,51 +33,46 @@ char * scrubHTML(const char *str){
       newStringLength += 3;
     }
     else if(str[i] == '"'){
-      newStringLength += 5;
+       newStringLength += 5;
     }
   }
 
   char *escapedArray = (char *) malloc((newStringLength + 1) * sizeof(char));
+  escapedArray[0] = '\0';
   int index = 0;
 
-  /*this for loop goes through the passed in string and finds where the special characters are an then replaces
-  them with the escaped charcters versions of them*/
+  /*
+  * this for loop goes through the passed in string and finds where the
+  * special characters are an then replaces them with the escaped charcters
+  * versions of them
+  */
+
   i = 0;
   for(i = 0; i < stringLength; i++){
     if(str[i] == '&'){
-      escapedArray[index] = '&';
-      escapedArray[index + 1] = 'a';
-      escapedArray[index + 2] = 'm';
-      escapedArray[index + 3] = 'p';
-      escapedArray[index + 4] = ';';
-      index = index + 5;
+      escapedArray = strcat(escapedArray, "&amp;");
+      index += 5;
+      escapedArray[index] = '\0';
     }
     else if(str[i] == '<'){
-      escapedArray[index] = '&';
-      escapedArray[index + 1] = 'l';
-      escapedArray[index + 2] = 't';
-      escapedArray[index + 3] = ';';
-      index = index + 4;
+      escapedArray = strcat(escapedArray, "&lt;");
+      index += 4;
+      escapedArray[index] = '\0';
     }
     else if(str[i] == '>'){
-      escapedArray[index] = '&';
-      escapedArray[index + 1] = 'g';
-      escapedArray[index + 2] = 't';
-      escapedArray[index + 3] = ';';
-      index = index + 4;
+      escapedArray = strcat(escapedArray, "&gt;");
+      index += 4;
+      escapedArray[index] = '\0';
     }
     else if(str[i] == '"'){
-      escapedArray[index] = '&';
-      escapedArray[index + 1] = 'q';
-      escapedArray[index + 2] = 'u';
-      escapedArray[index + 3] = 'o';
-      escapedArray[index + 4] = 't';
-      escapedArray[index + 5] = ';';
-      index = index + 6;
+      escapedArray = strcat(escapedArray, "&quot;");
+      index += 6;
+      escapedArray[index] = '\0';
     }
     else{
       escapedArray[index] = str[i];
       index++;
+      escapedArray[index] = '\0';
     }
   }
   escapedArray[index] = '\0';
@@ -89,11 +86,15 @@ char * smartScrubHTML(const char *str){
   }
 
   int stringLength = strlen(str);
-  char *escapedArray = (char *) malloc((stringLength * 6) * sizeof(char));
+  char *escapedArray = (char *) malloc(((stringLength * 6)) * sizeof(char));
+  escapedArray[0] = '\0';
 
-  /*this for loop goes through the string passed into the function to look for the special characters
-  if there is a special character it will be changed the escaped version. If the escaped version of one of
-  the special characters is in the string, it will stay like that.*/
+  /*
+  * this for loop goes through the string passed into the function to look for
+  * the special characters if there is a special character it will be changed the escaped version.
+  * If the escaped version of one of the special characters is in the string,
+  * it will stay like that.
+  */
 
   int index = 0;
   int i = 0;
@@ -101,180 +102,58 @@ char * smartScrubHTML(const char *str){
     if(str[i] == '&'){
       if(str[i + 1] == 'l' && str[i + 2] == 't' && str[i + 3] == ';'){
         escapedArray = strcat(escapedArray, "&lt;");
-        i = i + 3;
+        i += 3;
         index += 4;
+        escapedArray[index] = '\0';
       }
       else if(str[i + 1] == 'g' && str[i + 2] == 't' && str[i + 3] == ';'){
         escapedArray = strcat(escapedArray, "&gt;");
-        i = i + 3;
+        i += 3;
         index += 4;
       }
       else if(str[i + 1] == 'a' && str[i + 2] == 'm' && str[i + 3] == 'p' && str[i + 4] == ';'){
         escapedArray = strcat(escapedArray, "&amp;");
-        i = i + 4;
+        i += 4;
         index += 5;
+        escapedArray[index] = '\0';
       }
       else if(str[i + 1] == 'q' && str[i + 2] == 'u' && str[i + 3] == 'o' && str[i + 4] == 't' && str[i + 5] == ';'){
         escapedArray = strcat(escapedArray, "&quot;");
-        i = i + 5;
+        i += 5;
         index += 6;
+        escapedArray[index] = '\0';
       }
       else{
         escapedArray = strcat(escapedArray, "&amp;");
         index += 5;
+        escapedArray[index] = '\0';
       }
     }
     else if(str[i] == '<'){
       escapedArray = strcat(escapedArray, "&lt;");
       index += 4;
+      escapedArray[index] = '\0';
     }
     else if(str[i] == '>'){
       escapedArray = strcat(escapedArray, "&gt;");
       index += 4;
+      escapedArray[index] = '\0';
     }
     else if(str[i] == '"'){
       escapedArray = strcat(escapedArray, "&quot;");
       index += 6;
+      escapedArray[index] = '\0';
     }
     else{
       escapedArray[index] = str[i];
       index++;
+      escapedArray[index] = '\0';
     }
   }
 
   int newStringLength = strlen(escapedArray);
-
-  escapedArray[newStringLength] = '\0';
-
+  escapedArray[index] = '\0';
   return escapedArray;
-
-  //printf("input %s\n", str);
-  //this for loop goes through the passed in string and stringLengths the number of times
-  //one of the special characters shows up to calculate the extra amount of memory needed for the
-  //length of the new string.
-  //char testString[7];
-  // int newStringLength = 0;
-  // int i = 0;
-  // for(i = 0; i < stringLength; i++){
-  //   if(str[i] == '&'){
-  //
-  //
-  //     if(str[i + 1] == 'l' && str[i + 2] == 't' && str[i + 3] == ';'){
-  //       newStringLength = newStringLength + 4;
-  //       i = i + 4;
-  //     }
-  //     else if(str[i + 1] == 'g' && str[i + 2] == 't' && str[i + 3] == ';'){
-  //       newStringLength = newStringLength + 4;
-  //       i = i + 4;
-  //     }
-  //     else if(str[i + 1] == 'a' && str[i + 2] == 'm' && str[i + 3] == 'p' && str[i + 4] == ';'){
-  //       newStringLength = newStringLength + 5;
-  //       i = i + 5;
-  //     }
-  //     else if(str[i + 1] == 'q' && str[i + 2] == 'u' && str[i + 3] == 'o' && str[i + 4] == 't' && str[i + 5] == ';'){
-  //       newStringLength = newStringLength + 6;
-  //       i = i + 6;
-  //     }
-  //     else{
-  //       newStringLength = newStringLength + 5;
-  //     }
-  //   }
-  //   else if(str[i] == '<' || str[i] == '>'){
-  //     newStringLength = newStringLength + 4;
-  //   }
-  //   else if(str[i] == '"'){
-  //     newStringLength = newStringLength + 6;
-  //   }
-  //   else{
-  //     newStringLength++;
-  //   }
-  // }
-  // char *escapedArray = (char *) malloc((newStringLength + 1) * sizeof(char));
-  // int index = 0;
-  //
-  // i = 0;
-  // for(i = 0; i < stringLength; i++){
-  //   if(str[i] == '&'){
-  //     if(str[i + 1] == 'l' && str[i + 2] == 't' && str[i + 3] == ';'){
-  //
-  //       escapedArray[index] = '&';
-  //       escapedArray[index + 1] = 'l';
-  //       escapedArray[index + 2] = 't';
-  //       escapedArray[index + 3] = ';';
-  //       i = i + 3;
-  //       index = index + 4;
-  //     }
-  //     else if(str[i + 1] == 'g' && str[i + 2] == 't' && str[i + 3] == ';'){
-  //
-  //       escapedArray[index] = '&';
-  //       escapedArray[index + 1] = 'g';
-  //       escapedArray[index + 2] = 't';
-  //       escapedArray[index + 3] = ';';
-  //       i = i + 3;
-  //
-  //       index = index + 4;
-  //     }
-  //     else if(str[i + 1] == 'a' && str[i + 2] == 'm' && str[i + 3] == 'p' && str[i + 4] == ';'){
-  //       escapedArray[index] = '&';
-  //       escapedArray[index + 1] = 'a';
-  //       escapedArray[index + 2] = 'm';
-  //       escapedArray[index + 3] = 'p';
-  //       escapedArray[index + 4] = ';';
-  //       i = i + 4;
-  //       index = index + 5;
-  //     }
-  //     else if(str[i + 1] == 'q' && str[i + 2] == 'u' && str[i + 3] == 'o' && str[i + 4] == 't' && str[i + 5] == ';'){
-  //
-  //       escapedArray[index] = '&';
-  //       escapedArray[index + 1] = 'q';
-  //       escapedArray[index + 2] = 'u';
-  //       escapedArray[index + 3] = 'o';
-  //       escapedArray[index + 4] = 't';
-  //       escapedArray[index + 5] = ';';
-  //       i = i + 5;
-  //
-  //       index = index + 6;
-  //     }
-  //     else{
-  //     escapedArray[index] = '&';
-  //     escapedArray[index + 1] = 'a';
-  //     escapedArray[index + 2] = 'm';
-  //     escapedArray[index + 3] = 'p';
-  //     escapedArray[index + 4] = ';';
-  //     index = index + 5;
-  //     }
-  //   }
-  //   else if(str[i] == '<'){
-  //     escapedArray[index] = '&';
-  //     escapedArray[index + 1] = 'l';
-  //     escapedArray[index + 2] = 't';
-  //     escapedArray[index + 3] = ';';
-  //     index = index + 4;
-  //   }
-  //   else if(str[i] == '>'){
-  //     escapedArray[index] = '&';
-  //     escapedArray[index + 1] = 'g';
-  //     escapedArray[index + 2] = 't';
-  //     escapedArray[index + 3] = ';';
-  //     index = index + 4;
-  //   }
-  //   else if(str[i] == '"'){
-  //     escapedArray[index] = '&';
-  //     escapedArray[index + 1] = 'q';
-  //     escapedArray[index + 2] = 'u';
-  //     escapedArray[index + 3] = 'o';
-  //     escapedArray[index + 4] = 't';
-  //     escapedArray[index + 5] = ';';
-  //     index = index + 6;
-  //   }
-  //   else{
-  //     escapedArray[index] = str[i];
-  //     index++;
-  //   }
-  // }
-  // escapedArray[newStringLength] = '\0';
-  // return escapedArray;
-
 }
 
 void trim(char *str){
@@ -320,6 +199,7 @@ char * formatPhoneNumber(const char *phone){
   if(phone == NULL){
     return NULL;
   }
+
   int stringLength = strlen(phone);
   int formatedNumLen = 15;
   char *formatedNumber = (char *) malloc(formatedNumLen * sizeof(char));
@@ -342,8 +222,11 @@ char * formatPhoneNumber(const char *phone){
   if(strlen(allNums) < 10 || strlen(allNums) > 11){
     return NULL;
   }
-  //for numbers like 1-402-555-1234
-  //get rid of the one in front and move all the numbers down an index
+
+  /*
+  * for numbers like 1-402-555-1234 it gets rid of the one in front
+  * and moves all the numbers down an index
+  */
   int k = 0;
   if(strlen(allNums) == 11){
     for(k = 0; k < stringLength; k++){
